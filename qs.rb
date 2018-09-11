71efe9898abe02a1a6097fe8587ddd25b9df97bd
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-09-11 20:51:53 +0900

    Add version

--- qs.rb
+++ qs.rb
@@ -1,7 +1,8 @@
 RELEASE_VERSION='v0.2.0'
 class Qs < Formula
   desc "Support your git operation. Quck Squash!"
-  homepage ""
+  homepage "https://github.com/kamontia/qs.git"
+  version RELEASE_VERSION
   url "https://github.com/kamontia/qs/releases/download/#{RELEASE_VERSION}/qs-mac"
   sha256 "f2c0309aa74704424be9b9987544bc3e6cba44f16c0452e37d2c57813ac02ee1"
   # depends_on "cmake" => :build
