aebece54512dd01d51dd40c45596b9de5a290f25
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-09-11 21:03:56 +0900

    Fix packagename

--- qs.rb
+++ qs.rb
@@ -1,14 +1,15 @@
 RELEASE_VERSION='v0.2.0'
+PACKAGE_NAME='qs'
 class Qs < Formula
   desc "Support your git operation. Quck Squash!"
   homepage "https://github.com/kamontia/qs.git"
   version RELEASE_VERSION
-  url "https://github.com/kamontia/qs/releases/download/#{RELEASE_VERSION}/qs-mac"
+  url "https://github.com/kamontia/qs/releases/download/#{RELEASE_VERSION}/#{PACKAGE_NAME}"
   sha256 "f2c0309aa74704424be9b9987544bc3e6cba44f16c0452e37d2c57813ac02ee1"
   # depends_on "cmake" => :build
 
   def install
-    bin.install 'qs'
+    bin.install "#{PACKAGE_NAME}"
   end
 
   test do
