3f60ceabace715c698d82b1b6cdafd3cccd844af
Tatsuya Kamohara <17017563+kamontia@users.noreply.github.com>
2018-09-19 18:08:20 +0900

    Release v1.0.0

--- qs.rb
+++ qs.rb
@@ -1,11 +1,11 @@
-RELEASE_VERSION='v0.2.0'
+RELEASE_VERSION='v1.0.0'
 PACKAGE_NAME='qs'
 class Qs < Formula
   desc "Support your git operation. Quck Squash!"
   homepage "https://github.com/kamontia/qs.git"
   version RELEASE_VERSION
   url "https://github.com/kamontia/qs/releases/download/#{RELEASE_VERSION}/#{PACKAGE_NAME}"
-  sha256 "f2c0309aa74704424be9b9987544bc3e6cba44f16c0452e37d2c57813ac02ee1"
+  sha256 "9edda3c51a5b05a49e82fa233e37d737353b63c649ff7df5f4341a6c714ec53f"
   # depends_on "cmake" => :build
 
   def install
